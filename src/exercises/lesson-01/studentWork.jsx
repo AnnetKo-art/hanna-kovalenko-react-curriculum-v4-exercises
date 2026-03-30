35ebe0e6acf42b5a4e5692495fbbdfc0688bf057
Hanna Kovalenko <annet-ko@i.ua>
2026-03-29 20:18:57 -0500

    exercise lesson1

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,30 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Hanna';
+  const age = 45;
+  const hobbies = [
+    'Playing music instruments',
+    'Singing',
+    'Zumba dancing',
+    'Coding',
+    'AI Marketing',
+    'Developing Social media content',
+    'Travelling',
+  ];
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        {' '}
+        My name is {name} and I am {age} years old. These are my hobbies:
+      </p>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
